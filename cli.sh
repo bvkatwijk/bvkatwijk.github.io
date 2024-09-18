76ae6637def62cb15e737f127252a0b3f06bc221
Boris van Katwijk <bvkatwijk@gmail.com>
2024-09-18 13:45:56 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -4,10 +4,10 @@ set -euox pipefail
 CMD=$1
 
 obsidian_to_hugo () {
-    mkdir -p content/blog
+    rm -rf content/*
     python -m obsidian_to_hugo \
     --obsidian-vault-dir=docs \
-    --hugo-content-dir=content/blog
+    --hugo-content-dir=content
 }
 
 cmdRun () {
