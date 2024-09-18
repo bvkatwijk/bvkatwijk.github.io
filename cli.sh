fc3bc3046f9d5e3a9cc633883d81bfdc96ecadc5
Boris van Katwijk <bvkatwijk@gmail.com>
2024-09-18 11:50:27 +0200

    Update cli.sh

--- cli.sh
+++ cli.sh
@@ -7,7 +7,15 @@ cmdRun () {
     hugo server -D
 }
 
+cmdPublish () {
+    python -m obsidian_to_hugo \
+    --obsidian-vault-dir=docs \
+    --hugo-content-dir=content/blog
+    hugo --destination site
+}
+
 case $CMD in
     "run") cmdRun ;;
+    "publish") cmdPublish ;;
     *) echo "Unknown command $CMD"; exit 1 ;;
 esac
