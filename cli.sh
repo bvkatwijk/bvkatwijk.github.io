24af6afeec8d33817261e6df59178b327a397b82
Boris van Katwijk <bvkatwijk@gmail.com>
2024-09-28 11:23:53 +0200

    create issue

--- cli.sh
+++ cli.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euox pipefail
 
+OWNER_REPO=bvkatwijk/bvkatwijk.github.io
+
 set -a; source .env; set +a
 
 CMD=$1
@@ -11,6 +13,16 @@ obsidian_to_hugo () {
     python -m obsidian_to_hugo \
     --obsidian-vault-dir=vault \
     --hugo-content-dir=content
+}
+
+createIssue () {
+    curl -L \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/${OWNER_REPO}/issues \
+        -d '{"title":"Found a bug","body":"I'\''m having a problem with this.","labels":["bug"]}'
 }
 
 cmdRun () {
@@ -26,6 +38,7 @@ cmdPublish () {
 
 case $CMD in
     "run") cmdRun ;;
+    "issue") createIssue ;;
     "publish") cmdPublish ;;
     *) echo "Unknown command $CMD"; exit 1 ;;
 esac
