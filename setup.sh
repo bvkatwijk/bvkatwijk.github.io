4ace42a3940569b1c55bfaa803da0358264b648e
Boris van Katwijk <bvkatwijk@gmail.com>
2025-01-30 13:25:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# Required
-# hugo
-# gh
-
-# install hugo somehow
+asdf install
+brew install hugo
 pip install obsidian-to-hugo
 git clone https://github.com/budparr/gohugo-theme-ananke.git themes/ananke
 echo 'theme = "ananke"' >> config.toml
